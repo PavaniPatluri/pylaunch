49b88293eb1441053fe3656495f963eafdec7f09
Pavani Patluri <pavanipatluri45@gmail.com>
2025-12-24 13:38:09 +0530

    Bind app to Render port

--- app.py
+++ app.py
@@ -1583,4 +1583,4 @@ if __name__ == "__main__":
     sys.stdout = open(os.devnull, "w")
     sys.stderr = open(os.devnull, "w")
 
-    serve(app, host="127.0.0.1", port=5000)
+    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
